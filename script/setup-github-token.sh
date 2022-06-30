c4c2350702d02920c35b78ad37f58122829f5d2f
RADIUM\profe <trungtk.radium@gmail.com>
2022-07-01 00:47:51 +0700

    no message

--- script/setup-github-token.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-git config --global url."https://${{ secrets.GITHUB_TOKEN }}:x-oauth-basic@github.com/".insteadOf git@github.com:
-git config --global url."https://".insteadOf git://
